c21b0ac341a93f7df90e241b478eb5b033498d13
vasenkovstanislav <s.vasenkov@servers.com>
2019-08-27 23:44:52 +0300

    first tests

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,8 +3,8 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static helpers.SelenideHelper.$d;
--- src/test/java/pages/ShoppingListPage.java
+++ src/test/java/pages/ShoppingListPage.java
@@ -1,15 +1,6 @@
 package pages;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import pages.blocks.RegistrationBaseBlock;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$x;
 
 
 public class ShoppingListPage extends BasePage {
